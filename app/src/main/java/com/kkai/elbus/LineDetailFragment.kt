87a44dde57759dd014c482e0e372038f868aad24
Kkaiyuanwg <118371002+Kkaiyuanwg@users.noreply.github.com>
2024-06-15 05:29:12 +0200

    (in progress) adding line info

--- app/src/main/java/com/kkai/elbus/LineDetailFragment.kt
+++ app/src/main/java/com/kkai/elbus/LineDetailFragment.kt
@@ -1,18 +1,20 @@
 package com.kkai.elbus
 
+import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
-import android.os.Build
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.BackgroundColorSpan
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
-import androidx.core.os.BundleCompat.getSerializable
 import androidx.fragment.app.Fragment
 
 class DetailFragment : Fragment() {
@@ -21,7 +23,7 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.line_item_detail, container, false)
+        return inflater.inflate(R.layout.fragment_line_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -31,9 +33,15 @@ class DetailFragment : Fragment() {
         val squareText = view.findViewById<TextView>(R.id.square)
         val rounded = ContextCompat.getDrawable(requireContext(), R.drawable.rounded_bg)
 
+        val layout = view.findViewById<LinearLayout>(R.id.innerLayout)
+
         val bus = arguments?.getSerializable("bus", busesClass::class.java)
 
-        squareText?.text = "Linea " + bus?.nom_comer
+        val routesList: List<rutasClass>? = getStopsByBusId(bus?.id)
+        val firstroute = routesList?.get(0)
+        val secondroute = routesList?.get(1)
+
+        squareText?.text = bus?.nom_comer
 
         rounded?.colorFilter = PorterDuffColorFilter(
             getColorByNom(bus?.nom_comer.toString()),
@@ -44,6 +52,17 @@ class DetailFragment : Fragment() {
         val closeButton = view.findViewById<Button>(R.id.closelinebutton)
         closeButton.setOnClickListener {
             parentFragmentManager.popBackStack()
+        }
+
+        firstroute?.paradas?.forEach { stopId ->
+            val inflater = LayoutInflater.from(context)
+            val views = inflater.inflate(R.layout.line_item_detail, layout, false)
+
+            val textView = views.findViewById<TextView>(R.id.busStop)
+
+            textView.text = "${stopName(stopId.toString())} ${getLinksFromStopId(stopId)}"
+
+            layout.addView(views)
         }
 
     }
